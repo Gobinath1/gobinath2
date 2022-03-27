a6d99e16902ed0e9775db95c599476fc10427e2b
Gobinath1 <gobinathpalani3@gmail.com>
2022-03-27 13:07:44 +0530

    TaskDone

---
+++ src/com/git/Employee.java
@@ -0,0 +1,22 @@
+package com.git;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+	
+ public static void main(String[] args) {
+	 List<Integer>L = new ArrayList<>();
+	 
+	 L.add(10);
+	 L.add(20);
+	 L.add(30);
+	 L.add(40);
+	 L.add(50);
+	 
+	 System.out.println(L);
+	
+   }
+		
+	}
+
